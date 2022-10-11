2ac1fb60882d14651935f8397b733c7652147cdf
Colby Pate <cbpate08@gmail.com>
2022-10-11 14:38:10 -0500

    Finished View Product & Update Product

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Testing.Models;
+using System.Collections.Generic;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+
+    
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿
+using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using System.Runtime.CompilerServices;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+      private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("select * from products");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+                new { id = id });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("update products set name = @name, price = @price where ProductID = @ID", 
+                new {name = product.Name, price = product.Price, id = product.ProductID});
+        }
+
+
+    }
+}
